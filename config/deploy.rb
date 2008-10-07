76ec10a6d71578d7371940a415fc0ff93864dabc
Radical Designs <dev@radicaldesigns.org>
2008-10-07 15:00:31 -0700

    deploy scripts and blank schema

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,15 +1,35 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+require 'capistrano/ext/multistage'
+
+set :user, "gogreen"
+set :application, "gogreen"
+set :repository,  "git@github.com:radicaldesigns/gogreen.git"
 
 # If you aren't deploying to /u/apps/#{application} on the target
 # servers (which is the default), you can specify the actual location
 # via the :deploy_to variable:
-# set :deploy_to, "/var/www/#{application}"
+set :deploy_to, "/home/#{application}"
 
 # If you aren't using Subversion to manage your source code, specify
 # your SCM below:
-# set :scm, :subversion
+set :scm, :git
+set :deploy_via, :remote_cache
+set :git_enable_submodules, 1
 
-role :app, "your app-server here"
-role :web, "your web-server here"
-role :db,  "your db-server here", :primary => true
+after "deploy:update_code", "deploy:symlink_shared"
+after "deploy:update_code", "deploy:symlink_dbs"
+
+namespace :deploy do
+  task :start, :roles => :app do
+  end
+
+  task :stop, :roles => :app do
+  end
+
+  task :restart, :roles => :app do
+    invoke_command "touch #{current_path}/tmp/restart.txt"
+  end
+
+  task :symlink_shared, :roles => :app, :except => {:no_symlink => true} do
+    invoke_command "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+  end
+end
---
+++ config/deploy/staging.rb
@@ -0,0 +1,12 @@
+set :deploy_to, "/home/#{application}"
+
+role :app,  "gogreen.staging.radicaldesigns.org"
+role :web,  "gogreen.staging.radicaldesigns.org"
+role :db,   "gogreen.staging.radicaldesigns.org", :primary => true
+
+namespace :deploy do
+  task :symlink_dbs, :roles => :app, :except => {:no_symlink => true} do
+    invoke_command "ln -nfs #{shared_path}/db/development.sqlite3 #{release_path}/db/development.sqlite3"
+  end
+end
+
