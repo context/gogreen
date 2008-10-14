f757eee5b348612be80b2e56954dd7a88ca43319
Radical Designs <dev@radicaldesigns.org>
2008-10-13 17:17:22 -0700

    Fix up form so that it retains user input.

--- app/controllers/pledges_controller.rb
+++ app/controllers/pledges_controller.rb
@@ -4,7 +4,6 @@ class PledgesController < ApplicationController
   end
 
   def create
-    return if logged_in? #XXX: is this right? added for demo link.
     @pledge = Pledge.create(params[:pledge])
     if @pledge.valid?
       self.current_user = @pledge.user
