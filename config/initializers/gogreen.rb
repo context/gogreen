211c77b22ff719d1b5f52005900338a634cbb67f
Austin Putman <austin@radicaldesigns.org>
2008-10-13 17:23:58 -0700

    fix a spec

--- config/initializers/gogreen.rb
+++ config/initializers/gogreen.rb
@@ -5,7 +5,7 @@ module GoGreen
 end
 
 if File.exist? "#{RAILS_ROOT}/config/gogreen.yml"
-  YAML.load_file ("#{RAILS_ROOT}/config/gogreen.yml").each do |key, value|
+  YAML.load_file("#{RAILS_ROOT}/config/gogreen.yml").each do |key, value|
     GoGreen::Config.send "#{key}=", value
   end 
 else
--- spec/models/pledge_spec.rb
+++ spec/models/pledge_spec.rb
@@ -3,7 +3,8 @@ require File.dirname(__FILE__) + '/../spec_helper'
 describe Pledge do
   describe "associated user" do
     it "creates a user when given valid user attributes" do
-      pledge = new_pledge(:user => nil, :user_attributes => new_user.attributes.merge(:password => 'password', :password_confirmation => 'password'))
+      pledge = new_pledge :user => nil
+      pledge.user_attributes = new_user.attributes.merge(:password => 'password', :password_confirmation => 'password')
       pledge.save
       pledge.user.should_not be_new_record
     end
