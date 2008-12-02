c6d029e7acb8a931d5978f2f83ccc32c8ea6b564
Austin Putman <austin@zohar.(none)>
2008-12-02 15:13:19 -0800

    change rails version

--- config/environment.rb
+++ config/environment.rb
@@ -5,7 +5,7 @@
 # ENV['RAILS_ENV'] ||= 'production'
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.1.2' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.2.2' unless defined? RAILS_GEM_VERSION
 ENV["GEM_HOME"]="/home/radicaldesigns/.gems"
 ENV["GEM_PATH"]="/home/radicaldesigns/.gems:/usr/lib/ruby/gems/1.8"
 
